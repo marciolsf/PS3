43df92e8d7338e5a52b80db89bf8840007df32c5
Marcio Silveira <5767538+marciolsf@users.noreply.github.com>
2022-09-21 11:25:53 -0600

    Added SceneTool checker -- checks for missing files, objects, etc

---
+++ ps3home/SceneTool.ps1
@@ -0,0 +1,108 @@
+param
+(# Root folder for your USRDIR path
+    [Parameter()][string]$root_folder,
+    [Parameter()][switch]$Check_Scene,
+    [Parameter()][switch]$Check_SDC,
+    [Parameter()][switch]$Crawl_Scene_Assets
+)
+
+
+#$root_folder = "C:\bin\homeclient\NPIA00010\NPIA00010\USRDIR"
+$Scene_Found
+
+[xml]$base_home = get-content C:\Users\iamma\source\repos\PS3\ps3home\LOCALSCENELIST_BASIC.XML
+
+#$Check_Scene = $true
+#$Check_SDC = $true
+#$Crawl_Scene_Assets = $true
+
+foreach ($c in $base_home.SCENELIST.scene) {
+    Write-Verbose "Checking Scene [$($c.name)] on [$($c.config)]..." 
+
+    $ScenePath = "$($root_folder)\$($c.config)"
+    
+    <#using the -replace function works recursively, replacing all instances of the SCENE word, instead of just .SCENE
+    So here we just the .replace method instead
+    #>
+    $SDCPath = $ScenePath.Replace(".scene", ".SDC")
+
+    if ($Check_Scene) {
+        if (-not (Test-Path -path $ScenePath)) { 
+            Write-Host "Scene file not found --> $($ScenePath)" -ForegroundColor Red 
+            $Scene_Found = $false
+        }
+        else {
+            Write-Verbose "Scene file $($ScenePath) found"
+            $Scene_Found = $true
+        }
+    }
+
+    if ($Check_SDC) {
+        if (-not(Test-Path -Path $SDCPath)) {
+            write-verbose "Testing $($SDCPath)"
+            write-host "SDC file not found --> $($SDCPath)" -ForegroundColor Red
+        }
+        else {
+            Write-Verbose "SDC File $($SDCPath) found"
+        }
+    }
+
+    #only crawl the scene if it was found.
+    if ($Crawl_Scene_Assets -and $Scene_Found) {
+        
+        [xml]$assets = Get-Content $ScenePath 
+            
+        foreach ($a in $assets) {
+            #$AssetVersion = $a.game
+            #Write-Verbose $a.game -ForegroundColor Blue
+                    
+            $AssetPath = $a.game.assetFolder.asset.Source
+            foreach ($path in $assetPath) {
+                <#  The source attribute of the asset element indicates the location of the file.
+                            There's no consistency on how that's declared -- The following are all valid
+                            source="file:///resource_root/Build/environments -- notice the 3 slashes
+                            source="file://resource_root/Build/environments -- only 2 slashes
+                            source="environments/ -- no file prefix at all
+
+                            Some scenes are stored outside of build/environments (straight on the root), and that's ok! 
+                            But we still need to replace the path so we can validate
+                            source="file:///resource_root/CasinoMain/
+
+                            In all instances, the environments folder is under USRDIR, which is the true "Root" for the package. 
+                            For our path validator, we just need to replace the root references to something windows can understand
+
+                            First I add the root folder to any attributes that don't have it
+                            Then I replace 3 slashes to only 2 slashes
+                        #>
+                write-host "****** Original path $($path)" -ForegroundColor White
+                if ($path -inotlike "*build/environments*") {
+                    $path = $path -replace ("file://resource_root", "$($root_folder)/")
+                    write-host $path -ForegroundColor Green
+                }
+
+                if ($path -inotlike "file*") {
+                    $path = "$($root_folder)/$($path)"
+                    write-host $path -ForegroundColor blue
+                }                
+
+                $path = $path -replace ("file:///", "file://")
+                $path = $path -Replace ("file://resource_root/build/", "$($root_folder)/") 
+                        
+                if (-not(Test-Path -Path $path)) {
+                    write-host "Asset not found ---  $($path) not found" -ForegroundColor Red
+                }
+                <#else { 
+                            write-host "---  $($path) was found" -ForegroundColor Green
+                        }#>
+            }
+                    
+        }
+    }
+    #}
+    #}
+
+
+
+
+}
+
